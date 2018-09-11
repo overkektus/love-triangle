c740e6c03df8f1af343094628050f9c2e358a8e9
Piskunou Yahor <egor.piskunou@gmail.com>
2018-09-11 21:38:14 +0300

    done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,37 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  if(preferences) {
+    const graph = [];
+    
+    for(let i = 0; i < preferences.length + 1; i++) {
+      graph.push(new Array(preferences.length + 1));
+    }
+
+    for(let i = 0; i < graph.length; i++) {
+      for(let j = 0; j < graph[i].length; j++) {
+        graph[i][j] = 0;
+      }
+    }
+    
+    preferences.forEach((i, j) => {
+      graph[i][j+1] = 1;
+      graph[j+1][i] = 1;
+    });
+
+    let count = 0;
+    const n = graph.length;
+    for(let a = 0; a < n; a++) {
+      for(let b = a + 1; b < n; b++) {
+        for(let c = b + 1; c < n; c++) {
+          if(graph[a][b] !== 0 && graph[b][c] !== 0 && graph[a][c] !== 0) {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
+  } else {
+    return 0; 
+  }
 };
